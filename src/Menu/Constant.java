96f2910ff829e2fb6950f1232a5357759cb78fea
ThinhNguyen1204 <ThinhNgyenCTH@gmail.com>
2023-10-24 11:50:02 +0700

    dadadadad

---
+++ src/Menu/Constant.java
@@ -0,0 +1,6 @@
+package Menu;
+
+public class Constant {
+    public static int MAIN_MENU_WIDTH = 1280;
+    public static int MAIN_MENU_HEIGHT = 720;
+}
---
+++ src/Menu/MainMenuScene.java
@@ -0,0 +1,15 @@
+package Menu;
+
+import java.awt.*;
+
+public class MainMenuScene extends Scene{
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+}
