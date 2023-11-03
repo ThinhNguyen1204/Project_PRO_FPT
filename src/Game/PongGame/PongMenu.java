118180f46dd0d62d93462f479b8ed9530dea8402
ThinhNguyen1204 <ThinhNgyenCTH@gmail.com>
2023-11-03 14:36:38 +0700

    add package PongGame

---
+++ src/Game/PongGame/PongMenu.java
@@ -0,0 +1,19 @@
+package Game.PongGame;
+
+import Game.Menu.Scene;
+
+import java.awt.*;
+
+public class PongMenu extends Scene {
+
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+}
